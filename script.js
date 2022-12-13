bd3fb2285aa521f234a76ebab7b6e61a490e4019
bszeidl <szeidl.balazs@hotmail.com>
2022-12-13 15:02:25 +0100

    Flags are ready.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+
+    let flagWidth = 250;
+    let flagHeight = 50;
+    let canvasWidth = canvas.width;
+    let canvasHeight = canvas.height;
+
+    context.fillStyle = 'red';
+    context.fillRect(canvasWidth / 2 - flagWidth / 2,canvasHeight / 2 + flagHeight * -1.5,flagWidth,flagHeight);  
+
+    context.fillStyle = 'white';
+    context.fillRect(canvasWidth / 2 - flagWidth / 2,canvasHeight / 2 + flagHeight * -1.5 + 1* flagHeight,flagWidth,flagHeight);  
+
+
+    context.fillStyle = 'green';
+    context.fillRect(canvasWidth / 2 - flagWidth / 2,canvasHeight / 2 + flagHeight * -1.5 + 2* flagHeight,flagWidth,flagHeight);  
+
 
 }
 
